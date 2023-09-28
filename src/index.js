b5219ab052e5dae713ab0012ea58b6589e59c9ed
o <nesubludai@gmail.com>
2023-09-28 22:03:29 +0300

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
 
+import printMe from './print.js';
+
 function component() {
     const element = document.createElement('div');
 
-    // Lodash, now imported by this script
+    const btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+
+    btn.onclick = printMe;
+
+
+    element.appendChild(btn);
+
 
     return element;
 }
